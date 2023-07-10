0b3ee024549733dc6a0de1e99fb6207a47296b83
Shion <umaradri@student.21-school.ru>
2023-07-10 23:06:36 +0700

    add test for data initialized

--- src/glview.cpp
+++ src/glview.cpp
@@ -9,6 +9,7 @@ GlView::GlView(QWidget *parent)
 void GlView::send_data(obj_data file_data) {
     clear_data();
     data = file_data;
+    bool data_initialized = true;
 }
 
 void GlView::clear_data() {
@@ -32,21 +33,23 @@ void GlView::paintGL() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glColor4f(1.0f, 0.0f, 1.0f, 1.0f);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-1, 1, -1, 1, 1, 99999);
-    glTranslatef(0, 0, -2.8);
+    if (data_initialized) {
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glFrustum(-1, 1, -1, 1, 1, 99999);
+        glTranslatef(0, 0, -2.8);
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
 
-    for (int i = 0; i < data.count_surfaces; ++i) {
-        for (int j = 0; j < data.all_surfaces[i].amount_of_vertices; ++j) {
-            glBegin(GL_LINE_LOOP);
-            glVertex3d(all_surfaces[i].indices[j]->x,
-                       all_surfaces[i].indices[j]->y,
-                       all_surfaces[i].indices[j]->z);
+        for (int i = 0; i < data.count_surfaces; ++i) {
+            for (int j = 0; j < data.all_surfaces[i].amount_of_vertices; ++j) {
+                glBegin(GL_LINE_LOOP);
+                glVertex3d(all_surfaces[i].indices[j]->x,
+                           all_surfaces[i].indices[j]->y,
+                           all_surfaces[i].indices[j]->z);
+            }
+            glEnd();
         }
-        glEnd();
     }
 }
