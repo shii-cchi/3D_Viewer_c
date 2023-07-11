ba3cf46a48b8861719156969d74ed95ea7b7b014
Shion <umaradri@student.21-school.ru>
2023-07-11 20:23:47 +0700

    fix rotate

--- src/transform.c
+++ src/transform.c
@@ -1,43 +1,52 @@
 #include "transform.h"
+#include <stdio.h>
 
 void rotate(double degree_x, double degree_y, double degree_z, obj_data *data) {
+    printf("deg = %lf\n", degree_x);
     rotate_x(data, degree_x);
     rotate_y(data, degree_y);
     rotate_z(data, degree_z);   
 }
 
 void rotate_x(obj_data *data, double degree_x) {
-    degree_x = convert_to_rad(degree_x);
+    double degree_x_rad = convert_to_rad(degree_x);
     for(int i = 0; i < data->count_vertices; i++) {
-        data->all_vertices[i].y = data->all_vertices[i].y*cos(degree_x) 
-        - data->all_vertices[i].z*sin(degree_x);
-        data->all_vertices[i].z = data->all_vertices[i].y*sin(degree_x) 
-        + data->all_vertices[i].z*cos(degree_x);
+        double val_1 =  data->all_vertices[i].y;
+        double val_2 =  data->all_vertices[i].z;
+        data->all_vertices[i].y = val_1*cos(degree_x_rad) 
+        - val_2*sin(degree_x_rad);
+        data->all_vertices[i].z = val_1*sin(degree_x_rad) 
+        + val_2*cos(degree_x_rad);
     }
  }
 
 void rotate_y(obj_data *data, double degree_y) {
-    degree_y = convert_to_rad(degree_y);
+    double degree_y_rad = convert_to_rad(degree_y);
     for(int i = 0; i < data->count_vertices; i++) {
-        data->all_vertices[i].x = data->all_vertices[i].x*cos(degree_y) 
-        + data->all_vertices[i].z*sin(degree_y);
-        data->all_vertices[i].z = -data->all_vertices[i].x*sin(degree_y) 
-        + data->all_vertices[i].z*cos(degree_y);
+        double val_1 =  data->all_vertices[i].x;
+        double val_2 =  data->all_vertices[i].z;
+        data->all_vertices[i].x = val_1*cos(degree_y_rad) 
+        + val_2*sin(degree_y);
+        data->all_vertices[i].z = -val_1*sin(degree_y_rad) 
+        + val_2*cos(degree_y_rad);
     }
  }
 
 void rotate_z(obj_data *data, double degree_z) {
-    degree_z = convert_to_rad(degree_z);
+    double degree_z_rad = convert_to_rad(degree_z);
     for(int i = 0; i < data->count_vertices; i++) {
-        data->all_vertices[i].x = data->all_vertices[i].x*cos(degree_z) 
-        - data->all_vertices[i].y*sin(degree_z);
-        data->all_vertices[i].y = data->all_vertices[i].x*sin(degree_z) 
-        + data->all_vertices[i].y*cos(degree_z);
+        double val_1 =  data->all_vertices[i].x;
+        double val_2 =  data->all_vertices[i].y;
+        data->all_vertices[i].x = val_1*cos(degree_z_rad) 
+        - val_2*sin(degree_z_rad);
+        data->all_vertices[i].y = val_1*sin(degree_z_rad) 
+        + val_2*cos(degree_z_rad);
     }
  }
 
 double convert_to_rad(double degree) {
-    return (degree/360)*2*M_PI;
+    double deg = (degree/(double)360)*2*M_PI;
+    return deg;
 }
 
 void scale(double ratio, obj_data *data) {
